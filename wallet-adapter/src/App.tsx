e624e3f0f9c6c462bdfef30ee37fc9639f41a927
Tanishka <tanishkadeep09@gmail.com>
2025-01-06 02:14:18 +0530

    bug

--- wallet-adapter/src/App.tsx
+++ wallet-adapter/src/App.tsx
@@ -2,14 +2,22 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import {  WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  WalletDisconnectButton,
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import Airdrop from "./components/Airdrop";
 
 function App() {
   return (
-    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+    <ConnectionProvider
+      endpoint={
+        "https://solana-devnet.g.alchemy.com/v2/qYYs-Wc-EOSKQx3J0tQ4K5N--qi_BB5C"
+      }
+    >
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
           <WalletMultiButton />
