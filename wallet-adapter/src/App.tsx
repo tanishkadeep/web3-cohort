fbbd3330a3dc050cf17829968d11e1ba5ea20f0c
Tanishka <tanishkadeep09@gmail.com>
2025-01-06 01:39:07 +0530

    connect wallet

--- wallet-adapter/src/App.tsx
+++ wallet-adapter/src/App.tsx
@@ -1,5 +1,25 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {  WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+import Airdrop from "./components/Airdrop";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          <div>hi</div>
+          <Airdrop />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
 export default App;
---
+++ wallet-adapter/src/components/Airdrop.tsx
@@ -0,0 +1,10 @@
+const Airdrop = () => {
+  return (
+    <div>
+      <input type="number" placeholder="Amount" className="border-2 mr-4" />
+      <button className="border-2">Airdrop</button>
+    </div>
+  );
+};
+
+export default Airdrop;
